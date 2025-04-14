808ba9f1f6323d9eb9d52837dd26b1cac52e8776
GaetanoNicotra <nicotragaetano10@gmail.com>
2025-04-14 18:14:39 +0200

    [UPD] Scrittura in arrow function

--- name.js
+++ name.js
@@ -1,10 +1,12 @@
-function names(firstName, lastName) {
+const names = (firstName, lastName) => {
     const person = {
         nome: firstName,
         cognome: lastName
     };
     return person;
 }
-names('Gaetano', 'Nicotra')
+
+const risultato = names('Gaetano', 'Nicotra')
+console.log(risultato)
 
 module.exports = names;
