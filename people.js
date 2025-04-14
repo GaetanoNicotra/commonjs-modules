1b3066eb8214c84b560cbab24d6f2e76c6b9b55f
GaetanoNicotra <nicotragaetano10@gmail.com>
2025-04-14 19:37:58 +0200

    [UPD] terza consegna completata

--- people.js
+++ people.js
@@ -1,8 +1,17 @@
-const namesImported = require('./name')
-const hobbies = require('./hobbies')
+const namesImported = require('./name.js')
+const hobbies = require('./hobbies.js')
 
 const risultato = namesImported('Antonio', 'Cassano');
 console.log(risultato)
 
 const risultatoSecondo = hobbies('pesca', 'cucina', 'palestra')
 console.log(risultatoSecondo)
+
+const personInformation = () => {
+    const information = {
+        fullName: risultato,
+        passions: risultatoSecondo
+    };
+    return information;
+}
+console.log(personInformation());
