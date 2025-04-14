aa7484ebba13d8b90e3fe2dd4d1b7563b18233c5
GaetanoNicotra <nicotragaetano10@gmail.com>
2025-04-14 18:24:17 +0200

    [UPD] seconda funzione terminata

---
+++ hobbies.js
@@ -0,0 +1,7 @@
+const hobbies = (hobbyOne, hobbyTwo, hobbyThree) => {
+    const hobbiesArray = [hobbyOne, hobbyTwo, hobbyThree];
+    return hobbiesArray
+};
+
+const risultato = hobbies('calcio', 'nuoto', 'film');
+console.log(risultato)
